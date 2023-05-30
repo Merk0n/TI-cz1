bcfa7f472540913bccb1af9af65c0bb1eb16eeee
Merk0n <127611734+Merk0n@users.noreply.github.com>
2023-05-30 08:39:43 +0200

    i think its complete

--- index.php
+++ index.php
@@ -1,32 +1,46 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $hobbies = $_POST["hobbies"] ?? array();
-    $login = $_POST["login"] ?? "";
-    $password = $_POST["password"] ?? "";
-    $email = $_POST["email"] ?? "";
-  
-    // Check login and password length
-    if (strlen($login) < 5 || strlen($password) < 5) {
-      echo "Login and password must be at least 5 characters long.";
-      // Handle the error condition here
-    } elseif (empty($hobbies)) {
-      echo "Please select at least one hobby.";
-      // Handle the error condition here
-    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL) || !preg_match('/\.com$/', $email)) {
-      echo "Please enter a valid email address with a '.com' domain.";
-      // Handle the error condition here
-    } else {
-      // Hobbies are selected, login and password are valid, and email is valid
-      // Proceed with form processing
-      // Access other form fields using $_POST array
-      // ...
-      // Process the form data
-      // ...
-      echo "Form submitted successfully!";
-    }
-  }
-  
+<!DOCTYPE html>
+<html>
+<head>
+  <title>User Registration Form</title>
+  <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+  <h1>User Registration Form</h1>
+  <form action="process.php" method="post">
+    <label for="first-name">First Name:</label>
+    <input type="text" id="first-name" name="first-name" required><br><br>
 
+    <label for="last-name">Last Name:</label>
+    <input type="text" id="last-name" name="last-name" required><br><br>
 
+    <label for="login">Login:</label>
+    <input type="text" id="login" name="login" required><br><br>
 
-?>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required><br><br>
+
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email" required><br><br>
+
+    <label for="address">Address:</label>
+    <textarea id="address" name="address" required></textarea><br><br>
+
+    <label for="education">Education:</label>
+    <select id="education" name="education" required>
+      <option value="" disabled selected>Select education level</option>
+      <option value="Primary">Primary</option>
+      <option value="Secondary">Secondary</option>
+      <option value="Higher">Higher</option>
+    </select><br><br><br>
+
+    <label for="hobbies[]">Hobbies:</label>
+    <label><input type="checkbox" id="sports" name="hobbies[]" value="Sports">Sports</label>
+    <label><input type="checkbox" id="music" name="hobbies[]" value="Music">Music</label>
+    <label><input type="checkbox" id="travel" name="hobbies[]" value="Travel">Travel</label>
+    <label><input type="checkbox" id="movies" name="hobbies[]" value="Movies">Movies</label>
+    <label><input type="checkbox" id="cooking" name="hobbies[]" value="Cooking">Cooking</label><br><br>
+
+    <input type="submit" value="Register">
+  </form>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,80 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Retrieve form data
+  $firstName = $_POST['first-name'];
+  $lastName = $_POST['last-name'];
+  $login = $_POST['login'];
+  $password = $_POST['password'];
+  $email = $_POST['email'];
+  $address = $_POST['address'];
+  $education = $_POST['education'];
+  $hobbies = $_POST['hobbies'];
+
+  // Perform validations
+  $errors = [];
+
+  if (strlen($login) < 5 || strlen($password) < 5) {
+    $errors[] = "Login and password must be at least 5 characters long.";
+  }
+
+  if (!strpos($email, '.com')) {
+    $errors[] = "Email must contain '.com'.";
+  }
+
+  if (empty($hobbies)) {
+    $errors[] = "Please select at least one hobby.";
+  }
+
+
+  // If there are errors, display the error message and a back button
+  if (!empty($errors)) {
+    echo "<h2>Error:</h2>";
+    foreach ($errors as $error) {
+      echo "<p>$error</p>";
+    }
+    echo "<a href='index.php'>Go back</a>";
+    exit;
+  }
+  else{
+    echo "<h2>Registration Details:</h2>";
+    echo "<p><strong>First Name:</strong> " . $firstName . "</p>";
+    echo "<p><strong>Last Name:</strong> " . $lastName . "</p>";
+    echo "<p><strong>Login:</strong> " . $login . "</p>";
+    echo "<p><strong>Password:</strong> " . $password . "</p>";
+    echo "<p><strong>Email:</strong> " . $email . "</p>";
+    echo "<p><strong>Address:</strong> " . $address . "</p>";
+    echo "<p><strong>Education:</strong> " . $education . "</p>";
+    echo "<p><strong>Hobbies:</strong> " . (is_array($hobbies) ? implode(", ", $hobbies) : $hobbies) . "</p>";
+  }
+
+  $host = 'localhost';
+  $user = 'root';
+  $password = '';
+  $database = 'TI-cz1';
+  
+  $connection = mysqli_connect($host, $user, $password, $database);
+  
+  // Check connection
+  if (!$connection) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+  else {
+    echo "Connected successfully to $database <br>";
+  }
+  
+  // Prepare the SQL query
+  $sql = "INSERT INTO users (firstName, lastName, login, password, email, address, education, hobbies) 
+          VALUES ('$firstName', '$lastName', '$login', '$password', '$email', '$address', '$education', '$hobbies')";
+  
+  // Execute the query
+  if (mysqli_query($connection, $sql)) {
+      echo "Registration successful!";
+  } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($connection);
+  }
+  
+  // Close the database connection
+  mysqli_close($connection);
+
+}
+?>
